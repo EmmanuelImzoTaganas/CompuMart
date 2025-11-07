a113b134434e279c510c27fae35dbf2ea1c6f4b1
Zenhii2JZenhii <c.suela@pisdavao.com>
2025-11-07 20:54:00 +0800

    Update LoginController.java

--- src/main/java/compumart/compumart/controller/LoginController.java
+++ src/main/java/compumart/compumart/controller/LoginController.java
@@ -28,12 +28,12 @@ public class LoginController {
         String password = loginPasswordField.getText().trim();
 
         if (email.isEmpty() || password.isEmpty()) {
-            showAlert(Alert.AlertType.ERROR, "Error", "Please fill in all fields.");
+            showAlert(Alert.AlertType.ERROR, "Error!", "Please fill in all fields.");
             return;
         }
         boolean authenticated = userDao.authenticate(email, password);
         if (authenticated) {
-            showAlert(Alert.AlertType.INFORMATION, "Login Successful", "Welcome back!");
+            showAlert(Alert.AlertType.INFORMATION, "Login Successful!", "Welcome back!");
 
             // Navigate to products page after successful login
             navigateToProducts();
@@ -52,11 +52,11 @@ public class LoginController {
 
             } catch (IOException e) {
                 e.printStackTrace();
-                showAlert(Alert.AlertType.ERROR, "Error", "Unable to load main user view.");
+                showAlert(Alert.AlertType.ERROR, "Error!", "Unable to load main user view.");
             }
 
         } else {
-            showAlert(Alert.AlertType.ERROR, "Login Failed.", "Invalid email or password.");
+            showAlert(Alert.AlertType.ERROR, "Login Failed.", "Invalid email or password! ");
             loginPasswordField.clear();
         }
     }
@@ -94,7 +94,7 @@ public class LoginController {
 
         } catch (IOException e) {
             e.printStackTrace();
-            showAlert(Alert.AlertType.ERROR, "Navigation Error", "Unable to load products page.");
+            showAlert(Alert.AlertType.ERROR, "Navigation Error!", "Unable to load products page.");
         }
     }
 
